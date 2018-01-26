44bc7703443d2782b89f1071da0a5d0be90dc1c0
Selçuk Öztürk <mustafasozturk@gmail.com>
2018-01-26 17:59:03 +0300

    Final touches

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 //   
 function Replace_0(details) {    
-    return {redirectUrl: details.url.replace('wikipedia.org','0wikipedia.org')};
+    return {redirectUrl: details.url.replace('imgur.com','0imgur.com')};
 }
 
 chrome.webRequest.onBeforeRequest.addListener(Replace_0,
     {
         urls: [
-            "*://*.wikipedia.org/*"                
+            "*://*.imgur.com/*"                
         ],
         types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
     },
